859c140c105f95142456fb7b76e55d270bdcce56
Matthijs van Henten <matthijs@technischepartij.nl>
2015-08-24 22:03:06 +0000

    more functions; improve readme

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # eqne
 equal/not equal - functional pendants for javascript's === operator.
+
+## Install
+
+```bash
+    npm install
+```
+
+## Usage
+
+This library brings you functional versions of some useful javascript comparison operators,
+so  you can write nifty stuff like this:
+
+```javascript
+if( [1,2,3].all(eq(1)) )
+    console.log("They are all the same");
+    
+if( values.some(unlike(/\w+/) )
+    console.log("The input contains some invalid chars!");
+    
+if( stuff.every(lt(100)) )
+    console.log("Good health! all your values are below the treshold!);
+
+```
+
+## functions
+
+There's [test](./test/index.js)
+
+```javascript
+    var compare = require("eqne"),
+        eq = compare.eq,
+        ne = compare.ne,
+        lt = compare.lt,
+        gt = compare.gt,
+        like = compare.like,
+        unlie = cmpare.unlike;
+
+    // eq is non-strict equals ==
+    assert.ok(eq(1, 1), "1 == 1");
+    assert.ok([1,2,3].some(eq(1)), "some values are 1");
+
+    // me is non-strict non-equals !=
+    assert.ok(ne(1, 2), "1 != 2");
+    assert.ok([1,2,3].every(ne(3))), "no fives!");
+
+    // and additionally
+    assert.ok( like(/\d+/, 123) );
+    assert.ok( unlike(/\w+/, 123 );
+    assert.ok( lt(100, 2) );
+    assert.ok( gt(2, 100) );
+```
+
--- index.js
+++ index.js
@@ -1,31 +1,54 @@
 "use strict";
 
-function equals( a, b ){
-    return a == b;
-}
+var slice = require("sliced");
 
-function negate( fn ){
-    return function not(){
-        return !fn.apply( null, arguments );
+function compare(op) {
+    return function cmp(a, b) {
+        if (op == "eq") return a == b;
+        if (op == "gt") return a > b;
+        if (op == "lt") return a < b;
+        if (op == "re" && a instanceof RegExp)
+            return a.test(b);
+
+        return false;
     };
 }
 
-function wrap( a, compare ){
-    return function(value){
-        return compare( a, value );
+function not(fn) {
+    return function not() {
+        return !fn.apply(null, arguments);
     };
 }
 
-module.exports.eq = function eq( a, b ){
-    if( arguments.length === 1 )
-        return wrap( a, equals );
-        
-    return equals(a, b);
+function wrap(a, b, comparator) {
+    if (arguments.length === 3)
+        return comparator(a, b);
+
+    return function(value) {
+        return b(a, value);
+    };
+}
+
+module.exports.eq = function eq(a, b) {
+    return wrap.apply(null, slice(arguments).concat(compare("eq")));
 };
 
-module.exports.ne = function ne(a, b){
-    if( arguments.length === 1 )
-        return wrap( a, negate( equals ) );
-        
-    return ! equals( a, b );
+module.exports.ne = function ne(a, b) {
+    return wrap.apply(null, slice(arguments).concat(not(compare("eq"))));
+};
+
+module.exports.lt = function lt(a, b) {
+    return wrap.apply(null, slice(arguments).concat(compare("lt")));
+};
+
+module.exports.gt = function gt(a, b) {
+    return wrap.apply(null, slice(arguments).concat(compare("gt")));
+};
+
+module.exports.like = function like(a, b) {
+    return wrap.apply(null, slice(arguments).concat(compare("re")));
+};
+
+module.exports.unlike = function unlike(a, b) {
+    return wrap.apply(null, slice(arguments).concat(not(compare("re"))));
 };
